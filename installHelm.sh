dbc4fcabb91a7591a37cb0db7040f4401e969be1
cccaternberg <acaternberg@cloudbees.com>
2024-11-27 14:18:41 +0100

    add

--- installHelm.sh
+++ installHelm.sh
@@ -2,3 +2,5 @@
 
 helm install ci cloudbees/cloudbees-core -f values.yaml
 #helm install ci cloudbees/cloudbees-core -f values.yaml --dry-run --debug
+
+oc get route cjoc  -o jsonpath='{.status.ingress[0].routerCanonicalHostname}' | xargs dig
