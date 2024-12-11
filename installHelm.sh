48228dcb37e4ef00d68af5c590bb63a65b80361d
cccaternberg <acaternberg@cloudbees.com>
2024-12-11 11:25:56 +0100

    add

--- installHelm.sh
+++ installHelm.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+NAMESPACE=cb-ci
+oc new-project $NAMESPACE
+oc project $NAMESPACE
+
+helm repo add cloudbees https://public-charts.artifacts.cloudbees.com/repository/public/
+helm repo update
 helm install ci cloudbees/cloudbees-core -f values.yaml
 #helm install ci cloudbees/cloudbees-core -f values.yaml --dry-run --debug
 
